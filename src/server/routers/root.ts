61837663000017ecc9c378db54dbd40dbd61b929
James Wallington <jameswallington11@gmail.com>
2024-07-19 18:07:27 +0100

    feat: trpc setup

---
+++ src/server/routers/root.ts
@@ -0,0 +1,17 @@
+import { createRouter, publicProcedure } from "../trpc";
+import { z } from "zod";
+export const appRouter = createRouter({
+  hello: publicProcedure
+    .input(
+      z.object({
+        text: z.string(),
+      })
+    )
+    .query((opts) => {
+      return {
+        greeting: `hello ${opts.input.text}`,
+      };
+    }),
+});
+
+export type AppRouter = typeof appRouter;
