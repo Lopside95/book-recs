3719d2eb0af9bd2521ee268c66732b211652ac9b
James Wallington <jameswallington11@gmail.com>
2024-12-16 18:33:41 +0000

    add setBookRec on initial load

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,11 +29,16 @@ const FullRandom = () => {
       title: book?.title,
       rating: book?.rating,
     });
-    console.log("book", book);
   };
 
   useEffect(() => {
     refetch();
+    setBookRec({
+      author: book?.author.name,
+      genre: book?.genre.name,
+      title: book?.title,
+      rating: book?.rating,
+    });
   }, []);
 
   return (
